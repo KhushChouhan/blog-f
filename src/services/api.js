2885cce8c36ca7e5c7224425143a92c9956f7a78
Khushwant <khushchouhan9680@gmail.com>
2026-02-28 23:59:12 +0530

    Updated new features

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000/api/v1';
+const API_URL = 'https://blog-backend-q6gy.onrender.com/api/v1';
 
 const api = axios.create({
     baseURL: API_URL,
